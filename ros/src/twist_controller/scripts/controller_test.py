059a74516c7f2987ee04fd9e0956f5ef2e924db1
dmbelo <david.belo@mclaren.com>
2017-09-27 17:21:58 -0700

    Command line testing by specified setpoint velocity

--- ros/src/twist_controller/scripts/controller_test.py
+++ ros/src/twist_controller/scripts/controller_test.py
@@ -3,39 +3,76 @@
 import math
 
 import rospy
+from twist_controller.msg import VelocitySetpoint
 from geometry_msgs.msg import PoseStamped, Quaternion, TwistStamped
 from std_msgs.msg import Header
 
-def run():
-    rospy.init_node('controller_test')
-    loop()
 
-def loop():
+class ControllerTest(object):
+    def __init__(self):
+        rospy.init_node('controller_test')
+        self.velocity_setpoint = 0.0
+        self.setpoint_sub = rospy.Subscriber('/velocity_setpoint', VelocitySetpoint, self.velocity_setpoint_cb)
+        self.cmd_pub = rospy.Publisher('/twist_cmd', TwistStamped, queue_size=10)
+        self.loop()
 
-    rate = rospy.Rate(50)
+    
+    def loop(self):
+        rate = rospy.Rate(50)
 
-    while not rospy.is_shutdown():
+        while not rospy.is_shutdown():
+            t = rospy.Time.now()
+            # val = (5.0 * math.sin(1/5.0*2*math.pi*t.to_sec()) + 10) * 1.6 / 3.6
+            # val = 10 * 1.6 / 3.6
+            val = self.velocity_setpoint
+            rospy.loginfo('Speed demand: %f', val)
+
+            vel_cmd = TwistStamped()
+            vel_cmd.header = Header()
+            vel_cmd.header.stamp = t
+            vel_cmd.header.frame_id = '/vehicle'
+            vel_cmd.twist.linear.x = val
+
+            self.cmd_pub.publish(vel_cmd)
+
+            rate.sleep()
+
+
+    def velocity_setpoint_cb(self, msg):
+        self.velocity_setpoint = msg.velocity
+
+
+# def run():
+#     rospy.init_node('controller_test')
+#     loop()
+
+# def loop():
+
+#     rate = rospy.Rate(50)
+
+#     while not rospy.is_shutdown():
         
-        t = rospy.Time.now()
-        # val = (5.0 * math.sin(1/5.0*2*math.pi*t.to_sec()) + 10) * 1.6 / 3.6
-        val = 10 * 1.6 / 3.6
-        rospy.loginfo('Speed demand: %f', val)
+#         t = rospy.Time.now()
+#         # val = (5.0 * math.sin(1/5.0*2*math.pi*t.to_sec()) + 10) * 1.6 / 3.6
+#         # val = 10 * 1.6 / 3.6
+#         rospy.loginfo('Speed demand: %f', val)
 
-        vel_cmd = TwistStamped()
-        vel_cmd.header = Header()
-        vel_cmd.header.stamp = t
-        vel_cmd.header.frame_id = '/vehicle'
-        vel_cmd.twist.linear.x = val
+#         vel_cmd = TwistStamped()
+#         vel_cmd.header = Header()
+#         vel_cmd.header.stamp = t
+#         vel_cmd.header.frame_id = '/vehicle'
+#         vel_cmd.twist.linear.x = val
 
-        cmd_pub.publish(vel_cmd)
+#         cmd_pub.publish(vel_cmd)
 
-        rate.sleep()
+#         rate.sleep()
 
 
 if __name__ == '__main__':
-    cmd_pub  = rospy.Publisher('/twist_cmd', TwistStamped, queue_size=10)
+#     setpoint_sub = rospy.Subscriber('/velocity_cmd', VelocitySetpoint, self.speed_cb)
+#     cmd_pub = rospy.Publisher('/twist_cmd', TwistStamped, queue_size=10)
 
     try:
-        run()
+        ControllerTest()
     except rospy.ROSInterruptException:
         pass
